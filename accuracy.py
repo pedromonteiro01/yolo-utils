0f1eb42040b85e03c408d593ea84cad0d27ec8e7
Pedro Monteiro <64958453+pedromonteiro01@users.noreply.github.com>
2023-11-07 19:08:22 +0000

    add accuracy scripts

---
+++ accuracy.py
@@ -0,0 +1,93 @@
+import subprocess
+import os
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import yaml
+
+def evaluate_model(weight_file):
+    command = f"python3 yolov5/val.py --weights {weight_file} --data dataset.yaml --img 640 --conf 0.25 --iou 0.5"
+    result = subprocess.run(command, shell=True, text=True, capture_output=True)
+    
+    return result.stderr
+
+def extract_metrics(output):
+    mAP50 = 0.0
+    
+    for line in output.split("\n"):
+        if "all" in line:
+            metrics = line.split()
+            mAP50 = float(metrics[5])
+    
+    return mAP50
+
+def extract_details_from_opt(exp_path):
+    with open(os.path.join(exp_path, 'opt.yaml'), 'r') as f:
+        opt_data = yaml.safe_load(f)
+        epochs = opt_data['epochs']
+        batch_size = opt_data['batch_size']
+    return epochs, batch_size
+
+def find_best_config():
+    base_dir = "yolov5/runs/train"
+    
+    best_weight = None
+    best_mAP50 = 0.0
+    best_epochs = 0
+    best_batch_size = 0
+    
+    # lists to store values for plotting
+    mAP50_values = []
+    all_epochs = []
+    all_batch_sizes = []
+
+    # iterate through each exp directory
+    for exp_dir in os.listdir(base_dir):
+        if exp_dir.startswith("exp"):
+            current_dir = os.path.join(base_dir, exp_dir, "weights")
+            if not os.path.exists(current_dir):
+                continue
+            
+            epochs, batch_size = extract_details_from_opt(os.path.join(base_dir, exp_dir)) # get details from opt.yaml
+
+            print(f"Checking weight files in {current_dir}:")
+            print(f"epochs: {epochs} batch size: {batch_size}")
+
+            for filename in os.listdir(current_dir): # iterate through weight files in the current exp directory
+                if filename.endswith(".pt"):
+                    weight_path = os.path.join(current_dir, filename)
+
+                    output = evaluate_model(weight_path)
+                    mAP50 = extract_metrics(output)  # Extract mAP50 metric
+                    
+                    # store plot values
+                    mAP50_values.append(mAP50)
+                    all_epochs.append(epochs)
+                    all_batch_sizes.append(batch_size)
+
+                    if mAP50 > best_mAP50:
+                        best_mAP50 = mAP50
+                        best_weight = weight_path
+                        best_epochs = epochs
+                        best_batch_size = batch_size
+                        
+    return best_weight, best_mAP50, best_epochs, best_batch_size, mAP50_values, all_epochs, all_batch_sizes
+
+def plot_results(mAP50_values, epochs, batch_sizes):
+    plt.figure(figsize=(10, 6))
+    
+    # mAP50 vs. Epochs
+    scatter = plt.scatter(epochs, mAP50_values, c=batch_sizes, cmap='viridis', s=100, edgecolors='k')
+    plt.title('mAP50 vs. Epochs (Color-coded by Batch Size)')
+    plt.xlabel('Epochs')
+    plt.ylabel('mAP50')
+    cbar = plt.colorbar(scatter)
+    cbar.set_label('Batch Size')
+    
+    plt.tight_layout()
+    plt.savefig("mAP50_results.png")
+
+if __name__ == "__main__":
+    best_weight, best_mAP50, best_epochs, best_batch_size, all_mAP50_values, all_epochs, all_batch_sizes = find_best_config()
+    print(f"Best weight file: {best_weight} with mAP50: {best_mAP50}")
+    print(f"Number of Epochs: {best_epochs}, Batch Size: {best_batch_size}")
+    plot_results(all_mAP50_values, all_epochs, all_batch_sizes)
---
+++ accuracy_test.py
@@ -0,0 +1,32 @@
+import pytest
+import subprocess
+from unittest.mock import patch, mock_open
+from accuracy import evaluate_model, extract_metrics, extract_details_from_opt, find_best_config
+
+def test_evaluate_model():
+    # mock the subprocess.run to return a predetermined output
+    mocked_output = "Mocked stderr that would come from val.py"
+    with patch('subprocess.run') as mock_run:
+        mock_run.return_value = subprocess.CompletedProcess(args=[], returncode=0, stderr=mocked_output)
+        result = evaluate_model("fake_weights.pt")
+        assert result == mocked_output
+
+def test_extract_metrics():
+    # example output from val.py script
+    mocked_output = """
+    Class     Images  Instances          P          R      mAP50   mAP50-95: 
+    all         14         15       0.82      0.528       0.63       0.34
+    """
+    mAP50 = extract_metrics(mocked_output)
+    assert mAP50 == 0.63  # Based on the mocked output
+
+def test_extract_details_from_opt():
+    # mock the content of opt.yaml
+    mocked_yaml_content = """
+    epochs: 50
+    batch_size: 16
+    """
+    with patch("builtins.open", mock_open(read_data=mocked_yaml_content)):
+        epochs, batch_size = extract_details_from_opt("fake_path")
+        assert epochs == 50
+        assert batch_size == 16
