d7a4f30c314a4e7f3fb7b40b5a0d31fcc7866f01
Pedro Monteiro <64958453+pedromonteiro01@users.noreply.github.com>
2023-12-27 12:25:42 +0000

    Update iou2.py

--- iou2.py
+++ iou2.py
@@ -51,76 +51,95 @@ def compute_average_iou(detections, labels):
                     max_iou = iou
             iou_values.append(max_iou)
     if not iou_values:
-        print("No IoU values computed. There might be no detections or labels.")
-        return None
-    return sum(iou_values) / len(iou_values)
+        print("No IoU values computed for folder. There might be no detections or labels.")
+    return sum(iou_values) / len(iou_values) if iou_values else None
 
-
-def main(detect_folder=None, valid_folder=None):
-    if detect_folder is None or valid_folder is None:
-        parser = argparse.ArgumentParser(description='IoU Calculation')
-        parser.add_argument('--detect_folder', required=True, help='Path to detection folders')
-        parser.add_argument('--valid_folder', required=True, help='Path to validation label files')
-        args = parser.parse_args()
-        detect_folder = args.detect_folder
-        valid_folder = args.valid_folder
-
+def main(detect_folder, valid_folder, img_sizes):
     labels = get_sample_data(valid_folder)
+    results = []
 
-    results = [] # store IoU results along with experiment folder names
-
-    # iterate over all folders under the specified detect_folder
     for folder_name in os.listdir(detect_folder):
-        folder_path = os.path.join(detect_folder, folder_name)
+        if 'batch' in folder_name and 'epoch' in folder_name and 'img' in folder_name:
+            folder_path = os.path.join(detect_folder, folder_name)
+            batch_size, epoch_number, img_size = extract_parameters(folder_name)
 
-        # check if the folder name matches the pattern "batch*_epoch*"
-        if os.path.isdir(folder_path) and 'batch' in folder_name and 'epoch' in folder_name:
-            detect_labels_path = os.path.join(folder_path, 'labels')
-            detections = get_sample_data(detect_labels_path)
-            avg_iou = compute_average_iou(detections, labels)
-            if avg_iou is not None:
-                results.append((folder_path, avg_iou))
+            if img_size in img_sizes:
+                detect_labels_path = os.path.join(folder_path, 'labels')
+                
+                if os.path.isdir(detect_labels_path) and any(fname.endswith('.txt') for fname in os.listdir(detect_labels_path)):
+                    detections = get_sample_data(detect_labels_path)
+                    avg_iou = compute_average_iou(detections, labels)
+                    if avg_iou is not None:
+                        results.append((img_size, batch_size, epoch_number, avg_iou))
+                    else:
+                        print(f"No valid IoU for: {folder_name}")
+                else:
+                    print(f"No label files in: {detect_labels_path}")
+            else:
+                print(f"Skipping {folder_name} as it is not part of the specified image sizes.")
+        else:
+            print(f"Skipping {folder_name} as it does not match the expected pattern.")
 
-    # sort and display results by descending IoU
-    results.sort(key=lambda x: x[1], reverse=True)
-    for folder_path, avg_iou in results:
-        print(f"Evaluating folder: {folder_path}")
-        print(f"Average IoU for {folder_path}: {avg_iou:.2f}\n")
-
-    # plot the results
-    plot_iou_results(results)
+    if not results:
+        print("No IoU results to plot.")
+    else:
+        plot_iou_results(results)
 
 def extract_parameters(folder_name):
     batch_match = re.search(r'batch(\d+)', folder_name)
     epoch_match = re.search(r'epoch(\d+)', folder_name)
+    imgsize_match = re.search(r'img(\d+)', folder_name)
+    
     batch_size = int(batch_match.group(1)) if batch_match else None
     epoch_number = int(epoch_match.group(1)) if epoch_match else None
-    return batch_size, epoch_number
+    img_size = int(imgsize_match.group(1)) if imgsize_match else None
+
+    return batch_size, epoch_number, img_size
+
 
 def plot_iou_results(results):
-    # plot the IoU results against epoch numbers and batch sizes
-    batch_sizes = []
-    epoch_numbers = []
-    iou_values = []
+    if not results:
+        print("No IoU results to plot.")
+        return
+    
+    # plot the IoU results against epoch numbers, batch sizes, and image sizes
+    batch_sizes = [result[1] for result in results]
+    epoch_numbers = [result[2] for result in results]
+    img_sizes = [result[0] for result in results]
+    iou_values = [result[3] for result in results]
 
-    for folder_path, avg_iou in results:
-        batch_size, epoch_number = extract_parameters(folder_path)
-        if batch_size is not None and epoch_number is not None:
-            batch_sizes.append(batch_size)
-            epoch_numbers.append(epoch_number)
-            iou_values.append(avg_iou)
+    max_batch_size = max(batch_sizes)
+    min_batch_size = min(batch_sizes)
+    batch_size_range = max_batch_size - min_batch_size
+    batch_sizes_normalized = [(50 if batch_size_range == 0 else 
+                               (bs - min_batch_size) / batch_size_range * 100 + 10) 
+                              for bs in batch_sizes]
 
     plt.figure(figsize=(10, 6))
     
-    scatter = plt.scatter(epoch_numbers, iou_values, c=batch_sizes, cmap='viridis', s=100, edgecolors='k')
-    plt.title('Average IoU vs. Epoch Number (Color-coded by Batch Size)')
+    unique_img_sizes = list(set(img_sizes))
+    img_size_to_color = {size: i for i, size in enumerate(unique_img_sizes)}
+    colors = [img_size_to_color[size] for size in img_sizes]
+
+    scatter = plt.scatter(epoch_numbers, iou_values, c=colors, s=batch_sizes_normalized, cmap='viridis', edgecolors='k')
+    plt.title('Average IoU vs. Epoch Number (Color-coded by Image Size, Size-coded by Batch Size)')
     plt.xlabel('Epoch Number')
     plt.ylabel('Average IoU')
-    cbar = plt.colorbar(scatter)
-    cbar.set_label('Batch Size')
     
+    cbar = plt.colorbar(scatter, ticks=range(len(unique_img_sizes)))
+    cbar.ax.set_yticklabels([str(size) for size in unique_img_sizes])
+    cbar.set_label('Image Size')
+
+    if len(set(batch_sizes)) > 1:
+        handles, labels = [], []
+        for bs in sorted(set(batch_sizes)):
+            handles.append(plt.scatter([], [], s=((bs - min_batch_size) / batch_size_range * 100 + 10 if batch_size_range else 50), color='grey', alpha=0.7))
+            labels.append(f'Batch Size: {bs}')
+        plt.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, -0.15), scatterpoints=1, frameon=False, labelspacing=1, title='Batch Size', ncol=len(handles))
+
     plt.tight_layout()
-    plt.savefig("iou_results.png")
+    plt.savefig("iou_results_by_imgsize_and_batch.png")
+
 
 if __name__ == '__main__':
     main()
